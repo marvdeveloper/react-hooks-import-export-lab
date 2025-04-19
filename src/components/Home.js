98c33e33fe0bd78b079f6ca32802c271dd0f3b6f
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-19 17:03:22 +0300

    Import username and city from user.js and display in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { username, city } from "../data/user";  // Importing username and city
 
 function Home() {
   return (
@@ -9,3 +10,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;  // Don't forget to export the component!
